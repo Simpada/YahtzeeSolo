6a21c0b3cdf4f2477e5675ee8215efd9f57f2f72
Marcus <marcus1sp2@gmail.com>
2022-09-07 14:40:04 +0200

    🎮 creation of a main and Gamemanager class

---
+++ src/main/java/GameManager.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class GameManager {
+
+    Player player1 = new Player();
+
+    Player player2 = new Player();
+
+    private Die[] throwDice(Player currentPlayer) {
+
+        Random random = new Random();
+
+
+
+        return null;
+    }
+
+}
